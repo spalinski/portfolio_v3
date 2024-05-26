fdcbe4bb669cdf90543f98180ff68f7f77b7bbc1
Michał Spaliński <mspalinski@gmail.com>
2024-05-26 10:08:24 +0200

    initial commit

---
+++ src/constants.js
@@ -0,0 +1,2 @@
+export const SITE_TITLE = "MIRIAM SPALINSKI";
+export const SITE_DESCRIPTION = "Portfolio";
---
+++ tailwind.config.mjs
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        jost: ["Jost", "sans-serif"],
+      },
+      fontWeight: {
+        100: "100",
+        200: "200",
+        300: "300",
+        400: "400",
+        500: "500",
+        600: "600",
+        700: "700",
+        800: "800",
+        900: "900",
+      },
+      colors: {
+        "msky": "#E2EBFE",
+        "mred": "#DD466F",
+        "msun": "#FDFAEF",
+      },
+    },
+},
+  plugins: [require("flowbite/plugin"), require("@tailwindcss/typography")],
+  // darkMode: "class",
+};
